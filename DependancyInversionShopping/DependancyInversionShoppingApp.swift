99fb394852eed0a735d1bfce7c3bd1301875eabf
Pac12Run8000 <gangstarr78992@>
2023-02-17 04:11:53 -0800

    Initial Commit

---
+++ DependancyInversionShopping/DependancyInversionShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependancyInversionShoppingApp.swift
+//  DependancyInversionShopping
+//
+//  Created by Michelle Grover on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DependancyInversionShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
